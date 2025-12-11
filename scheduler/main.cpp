61f19b85b31c4d9fb205091da9c4cd95b6cb5158
akioweh <0@akioweh.com>
2025-12-11 16:45:16 +0000

    no magic constants anymore
    heheheha

--- scheduler/main.cpp
+++ scheduler/main.cpp
@@ -2,11 +2,14 @@
 
 using namespace drogon;
 
+constexpr auto PORT = 80;
+constexpr auto N_THREADS = 8;
+
 auto main() -> int {
     app()
         .setLogPath("./")
         .setLogLevel(trantor::Logger::kWarn)
-        .addListener("0.0.0.0", 80)
-        .setThreadNum(16)
+        .addListener("0.0.0.0", PORT)
+        .setThreadNum(N_THREADS)
         .run();
 }
