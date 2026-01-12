92295bbe254de6ea245c52de9f153438bea96b18
maksiu <maksymilian.sieklinski.24@ucl.ac.uk>
2026-01-12 21:30:40 +0000

    created a utils class for usefull overall functions

---
+++ scheduler/src/utils/Utils.cpp
@@ -0,0 +1,47 @@
+#include "Utils.hpp"
+
+using namespace std;
+using namespace drogon; 
+
+auto Utils::parseTimestampSeconds(const string &timestamp) -> long long 
+{
+    // chatgpted - later i can try importing Howard Hinnant’s date library
+    // also i will probably move this to a utils package
+    string datetime = timestamp.substr(0, 19);
+    tm tm = {};
+    istringstream ss(datetime);
+    ss >> get_time(&tm, "%Y-%m-%dT%H:%M:%S");
+    time_t tt = timegm(&tm); 
+    return static_cast<long long>(tt);
+}
+
+auto Utils::makeGetRequest(const string &host, const string &path)
+    -> Task<shared_ptr<Json::Value>> {
+    auto client = HttpClient::newHttpClient(host);
+    auto request = HttpRequest::newHttpRequest();
+    request->setMethod(Get);
+    request->setPath(path);
+
+    HttpResponsePtr response;
+    try {
+        response = co_await client->sendRequestCoro(request);
+
+    } catch (const exception &e) {
+        LOG_ERROR << "something is not yes, maybe run python API? XD "
+                  << e.what();
+        co_return NULL;
+    }
+
+    if (!response || response->getStatusCode() != drogon::k200OK) {
+        LOG_ERROR << "response is null or has a code different than 200" ;
+        co_return NULL;
+    }
+
+    auto jsonResponsePtr = response->jsonObject();
+    if (!jsonResponsePtr) {
+        LOG_ERROR << "couldnt transform to JSON the response" ;
+        co_return NULL;
+    }
+    
+    co_return make_shared<Json::Value>(*jsonResponsePtr);
+}
