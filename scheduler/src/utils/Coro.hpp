08f21d028da9bf45d4e06c8639782b984d392acc
akioweh <0@akioweh.com>
2026-01-17 22:11:37 +0000

    give when_all a pack expansion-based tuple overload
    i have ZERO idea if this works

--- scheduler/src/utils/Coro.hpp
+++ scheduler/src/utils/Coro.hpp
@@ -1,63 +1,112 @@
-#ifndef CORO
-#define CORO
+/*
+ * i sure know what i am doing
+ */
 
+#ifndef SCHEDULER_UTIL_CORO
+#define SCHEDULER_UTIL_CORO
+#pragma once
+
+#include "utils/Utils.hpp"
 #include <atomic>
 #include <coroutine>
 #include <cstddef>
 #include <drogon/drogon.h>
 #include <drogon/utils/coroutine.h>
-#include <vector>
+#include <expected>
+#include <tuple>
 
 namespace scheduler::coro {
 namespace detail {
-template <typename T> class Handler {
+
+template <typename T> struct VectorResultContext {
+    std::atomic<size_t> remaining;
+    std::coroutine_handle<> continuation;
     std::vector<T> results;
-    std::coroutine_handle<> whenAllHandle;
-    std::atomic<size_t> activeJobsCounter;
 
-  public:
-    Handler(size_t numberOfJobs) : activeJobsCounter(numberOfJobs) {
-        results.resize(numberOfJobs);
+    VectorResultContext(size_t cnt) : remaining(cnt), results(cnt) {}
+};
+
+template <typename... Ts> struct ResultContext {
+    std::atomic<size_t> remaining;
+    std::coroutine_handle<> continuation;
+    std::tuple<Ts...> results;
+
+    ResultContext(size_t cnt) : remaining(cnt) {}
+};
+
+template <typename Context>
+    requires requires(Context ctx) {
+        { ctx.remaining.load() } -> std::convertible_to<size_t>;
+        { ctx.continuation } -> std::convertible_to<std::coroutine_handle<>>;
+        { ctx.results };
     }
+struct Awaiter {
+    std::shared_ptr<Context> ctx;
 
-    auto await_ready() -> bool { return false; }
-
-    auto await_suspend(std::coroutine_handle<> injectedHandle) {
-        whenAllHandle = injectedHandle;
+    auto await_ready() -> bool { return ctx->remaining.load() == 0; }
+    auto await_suspend(std::coroutine_handle<> handle) {
+        ctx->continuation = handle;
     }
-
-    auto await_resume() -> std::vector<T> { return std::move(results); }
-
-    auto jobFinished() {
-        if (activeJobsCounter.fetch_sub(1) == 1)
-            resumeWhenAll();
-    }
-
-    auto resumeWhenAll() { whenAllHandle.resume(); }
-
-    auto saveResult(size_t index, T value) {
-        results[index] = std::move(value);
+    auto await_resume() -> decltype(ctx->results) {
+        return std::move(ctx->results);
     }
 };
+
 } // namespace detail
 
-template <typename T>
-auto when_all(std::vector<drogon::Task<T>> jobs)
-    -> drogon::Task<std::vector<T>> {
-    auto numberOfJobs = jobs.size();
-    auto manager = std::make_shared<detail::Handler<T>>(numberOfJobs);
+template <typename... Rets, bool return_exceptions = false,
+          size_t N = sizeof...(Rets)>
+auto when_all(drogon::Task<Rets>... coros)
+    -> drogon::Task<std::tuple<std::conditional_t<
+        return_exceptions, std::expected<Rets, std::exception_ptr>, Rets>...>> {
+    auto ctx = std::make_shared<detail::ResultContext<std::conditional_t<
+        return_exceptions, std::expected<Rets, std::exception_ptr>, Rets>...>>(
+        N);
 
-    for (int index = 0; index < numberOfJobs; index++) {
-        drogon::async_run([index, &jobs, manager]() -> drogon::Task<void> {
-            auto job = std::move(jobs[index]);
-            manager->saveResult(index, co_await job);
-            manager->jobFinished();
-            co_return;
+    [&]<std::size_t... Is>(std::index_sequence<Is...>) -> auto {
+        ((drogon::async_run(
+             [task = std::move(coros), ctx]() mutable -> drogon::Task<void> {
+                 if constexpr (return_exceptions)
+                     std::get<Is>(ctx->results) =
+                         utils::pcall(drogon::async_func(task));
+                 else
+                     std::get<Is>(ctx->results) = co_await task;
+
+                 if (ctx->remaining.fetch_sub(1) == 1)
+                     ctx->continuation.resume();
+             })),
+         ...);
+    }(std::make_index_sequence<N>{});
+
+    co_return co_await detail::Awaiter{ctx};
+}
+
+template <typename T, bool return_exceptions = false>
+auto when_all(std::vector<drogon::Task<T>> coros)
+    -> drogon::Task<std::vector<std::conditional_t<
+        return_exceptions, std::expected<T, std::exception_ptr>, T>>> {
+    const auto n = coros.size();
+    if (!n)
+        co_return {};
+
+    auto ctx = std::make_shared<detail::VectorResultContext<T>>(n);
+
+    for (auto i = 0UZ; i < n; ++i) {
+        drogon::async_run([i, task = std::move(coros[i]),
+                           ctx]() mutable -> drogon::Task<void> {
+            if constexpr (return_exceptions) // idk...
+                ctx->results[i] = utils::pcall(drogon::async_func(task));
+            else
+                ctx->results[i] = co_await task;
+
+            if (ctx->remaining.fetch_sub(1) == 1)
+                ctx->continuation.resume();
         });
     }
 
-    co_return co_await *manager;
+    co_return co_await detail::Awaiter{ctx};
 }
+
 } // namespace scheduler::coro
 
-#endif
+#endif // SCHEDULER_UTIL_CORO
--- scheduler/src/utils/Utils.hpp
+++ scheduler/src/utils/Utils.hpp
@@ -16,6 +16,24 @@ auto toIso8601(const std::chrono::system_clock::time_point &timePoint)
 
 auto makeGetRequest(const std::string &host, const std::string &path)
     -> drogon::Task<std::shared_ptr<Json::Value>>;
+
+// "Protected" call that captures exceptions if any are thrown
+// using std::expected
+template <typename Func>
+    requires std::is_invocable_v<Func>
+auto pcall(Func &&func) -> std::expected<decltype(func()), std::exception_ptr> {
+    using return_t = decltype(func());
+    try {
+        if constexpr (std::is_void_v<return_t>) {
+            func();
+            return {};
+        } else {
+            return func();
+        }
+    } catch (...) {
+        return std::unexpected(std::current_exception());
+    }
+}
 }; // namespace scheduler::utils
 
 #endif
