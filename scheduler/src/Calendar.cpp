2fe52cd2504b08a0723c63aeb981ac50416fee94
maksiu <maksymilian.sieklinski.24@ucl.ac.uk>
2026-01-30 15:42:23 +0000

    connecting to the DB in progress

--- scheduler/src/Calendar.cpp
+++ scheduler/src/Calendar.cpp
@@ -13,11 +13,35 @@ using ImpactMapper = drogon::orm::Mapper<ImpactModel>;
 
 auto CalendarService::add(const std::string &jobId, Schedule schedule) -> void {
     std::unique_lock lock(mutex);
-    calendar[jobId] = std::move(schedule);
     
     const auto dbPtr = drogon::app().getFastDbClient(); 
     ImpactMapper impactMapper(dbPtr) ;
     JobMapper jobsMapper(dbPtr) ;
+
+    const auto& [impact, intervals] = schedule; 
+
+    ImpactModel impactDB;
+    impactDB.setCarbonIntensity(impact.carbon_intensity) ;
+    impactDB.setSci(impact.sci) ;
+    impactDB.setTotalEmissions(impact.total_emissions) ;
+    // can refactor this later to other function
+
+    impactMapper.insert(impactDB) ;
+    const int impactId = *impactDB.getId() ;
+
+    std::vector<JobModel> jobsBatch ;
+    for(const auto& interval: intervals)
+    {
+        JobModel job; 
+        job.setAdditionalLoad(interval.additionalLoad) ;
+        job.setTotalLoad(interval.totalLoad) ;
+        job.setLocationId(interval.location) ;
+        job.setImpactId(impactId) ;
+        /// this can also be refactored
+        jobsBatch.push_back(std::move(job)) ;
+    }
+
+    calendar[jobId] = std::move(schedule);
 }
 
 auto CalendarService::get(const std::string &jobId) const
