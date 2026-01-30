97a73baa6657c1d4bef890cca93c36dfbe1ef2b4
maksiu <maksymilian.sieklinski.24@ucl.ac.uk>
2026-01-30 15:31:10 +0000

    making using the models easier

--- scheduler/src/Calendar.cpp
+++ scheduler/src/Calendar.cpp
@@ -2,10 +2,22 @@
 #include "Scheduler.hpp"
 #include <expected>
 #include <mutex>
+#include <models/Impacts.h>
+#include <models/Jobs.h>
+
+using JobModel = drogon_model::calendar_db::Jobs ;
+using ImpactModel = drogon_model::calendar_db::Impacts ;
+
+using JobMapper = drogon::orm::Mapper<JobModel>;
+using ImpactMapper = drogon::orm::Mapper<ImpactModel>;
 
 auto CalendarService::add(const std::string &jobId, Schedule schedule) -> void {
     std::unique_lock lock(mutex);
     calendar[jobId] = std::move(schedule);
+    
+    const auto dbPtr = drogon::app().getFastDbClient(); 
+    ImpactMapper impactMapper(dbPtr) ;
+    JobMapper jobsMapper(dbPtr) ;
 }
 
 auto CalendarService::get(const std::string &jobId) const
