58994c9b15b8ab9ee4e937e588b2807bccf12a54
maksiu <maksymilian.sieklinski.24@ucl.ac.uk>
2026-01-30 15:21:52 +0000

    cleanup main

--- scheduler/main.cpp
+++ scheduler/main.cpp
@@ -16,37 +16,12 @@ using namespace drogon;
 constexpr auto PORT = 6969;
 constexpr auto N_THREADS = 8;
 
-auto generateJobRequest(int seed) -> JobRequest {
-    // using arbitrary timestamps
-    auto now = std::chrono::system_clock::now();
-    auto later = now + std::chrono::hours(24);
-    return {"training", 1469000.0, now, later, "job-" + std::to_string(seed)};
-}
-
 auto main() -> int {
     drogon::app().setLogPath(".");
-
     drogon::app().loadConfigFile("config.json");
-    
-    /*
-    drogon::async_run([&]() -> drogon::Task<> {
-        auto [impact, dummy] =
-            co_await schedulingQueue->computeSchedule(generateJobRequest(1));
-        std::cout << "Expected carbon emmissions: " << impact.total_emissions
-                  << '\n';
-        std::cout << "Expected carbon intensity: " << impact.carbon_intensity
-                  << '\n';
-        std::cout << "SCI: " << impact.sci << '\n';
-
-        for (auto i : dummy) {
-            i.show();
-        }
-        drogon::app().quit();
-        co_return;
-    });
-    */
     drogon::app().addListener("0.0.0.0", PORT);
     drogon::app().run();
+
 #ifdef _WIN32
     TerminateProcess(GetCurrentProcess(), 0);
 #endif // _WIN32
