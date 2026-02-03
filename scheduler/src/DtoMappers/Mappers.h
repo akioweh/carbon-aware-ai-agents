4f5a9fc8cbc9a3f42ce6770e54fc0ae0effe9bf8
maksiu <maksymilian.sieklinski.24@ucl.ac.uk>
2026-02-03 20:34:01 +0000

    added a convienience mapper of a collection

--- scheduler/src/DtoMappers/Mappers.h
+++ scheduler/src/DtoMappers/Mappers.h
@@ -1,41 +1,52 @@
-#ifndef MAPPERS
-#define MAPPERS
+    #ifndef MAPPERS
+    #define MAPPERS
 
-#include "structs/ScheduleBlock.hpp"
-#include <Scheduler.hpp>
-#include <models/Impacts.h>
-#include <models/Jobs.h>
+    #include "structs/ScheduleBlock.hpp"
+    #include <Scheduler.hpp>
+    #include <concepts>
+    #include <models/Impacts.h>
+    #include <models/Jobs.h>
 
-namespace mappers {
-using JobModel = drogon_model::calendar_db::Jobs;
-using ImpactModel = drogon_model::calendar_db::Impacts;
-using JobMapper = drogon::orm::Mapper<JobModel>;
-using ImpactMapper = drogon::orm::Mapper<ImpactModel>;
+    namespace mappers {
+    using JobModel = drogon_model::calendar_db::Jobs;
+    using ImpactModel = drogon_model::calendar_db::Impacts;
+    using JobMapper = drogon::orm::Mapper<JobModel>;
+    using ImpactMapper = drogon::orm::Mapper<ImpactModel>;
 
-auto f_toDto(ScheduleImpact) -> ImpactModel;
-auto f_toDto(const ScheduleBlock &, int) -> JobModel;
-auto f_fromDto(ImpactModel) -> ScheduleImpact;
-auto f_fromDto(JobModel) -> ScheduleBlock;
+    auto f_toDto(ScheduleImpact) -> ImpactModel;
+    auto f_toDto(const ScheduleBlock &, int) -> JobModel;
+    auto f_fromDto(ImpactModel) -> ScheduleImpact;
+    auto f_fromDto(JobModel) -> ScheduleBlock;
 
-struct FromDtoFn {
-    auto operator()(auto &&dto) const {
-        return f_fromDto(std::forward<decltype(dto)>(dto));
+    struct FromDtoFn {
+        auto operator()(auto &&dto) const {
+            return f_fromDto(std::forward<decltype(dto)>(dto));
+        }
+    };
+
+    struct ToDtoFn {
+        auto operator()(const ScheduleImpact &impact) const {
+            return f_toDto(impact);
+        }
+
+        [[nodiscard]] auto withImpactId(int impactId) const {
+            return [impactId](const ScheduleBlock &block) {
+                return f_toDto(block, impactId);
+            };
+        }
+    };
+
+    inline constexpr FromDtoFn fromDto{};
+    inline constexpr ToDtoFn toDto{};
+
+    // Convienience so i dont repeat myself.
+    template <typename T>
+    concept MappableModel =
+        std::same_as<T, JobModel> || std::same_as<T, ImpactModel>;
+    template <MappableModel T> auto fromDtoAll(const std::vector<T> &models) {
+        return models | std::views::transform(fromDto) |
+            std::ranges::to<std::vector>();
     }
-};
 
-struct ToDtoFn {
-    auto operator()(const ScheduleImpact &impact) const {
-        return f_toDto(impact);
-    }
-
-    [[nodiscard]] auto withImpactId(int impactId) const {
-        return [impactId](const ScheduleBlock &block) {
-            return f_toDto(block, impactId);
-        };
-    }
-};
-
-inline constexpr FromDtoFn fromDto{};
-inline constexpr ToDtoFn toDto{};
-}; // namespace mappers
-#endif
+    }; // namespace mappers
+    #endif
