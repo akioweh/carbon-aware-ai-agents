61e483170922dcfc6d74c6f52b8351e45836f837
maksiu <maksymilian.sieklinski.24@ucl.ac.uk>
2026-02-03 23:25:37 +0000

    cleaning up code, and adding when_all

--- scheduler/src/Calendar.cpp
+++ scheduler/src/Calendar.cpp
@@ -3,88 +3,93 @@
 #include <DtoMappers/Mappers.h>
 #include <Scheduler.hpp>
 #include <drogon/orm/Exception.h>
-#include <mutex>
 #include <ranges>
+#include <utils/Coro.hpp>
 #include <utils/Utils.hpp>
 
-auto CalendarService::add(Schedule schedule) -> drogon::Task<> {
-    std::unique_lock lock(mutex);
+namespace calendarService {
 
-    const auto transaction =
-        co_await drogon::app().getDbClient()->newTransactionCoro();
-    mappers::ImpactMapper impactMapper(transaction);
-    mappers::JobMapper jobsMapper(transaction);
+namespace {
 
+struct Context {
+    mappers::ImpactMapper impactMapper;
+    mappers::JobMapper jobsMapper;
+    explicit Context(const drogon::orm::DbClientPtr &db)
+        : impactMapper(db), jobsMapper(db) {}
+};
+
+auto getContex() { return Context(drogon::app().getDbClient()); }
+
+auto jobImpactIdEqualityCriteria(const int impactId) {
+    return drogon::orm::Criteria(mappers::JobModel::Cols::_impact_id,
+                                 drogon::orm::CompareOperator::EQ, impactId);
+}
+
+// I will add criteria for the timestamps here
+
+} // namespace
+
+auto add(Schedule schedule) -> drogon::Task<> {
+    auto transaction = co_await drogon::app().getDbClient()->newTransactionCoro();
+    Context context(transaction) ;
     const auto &[impact, intervals] = schedule;
 
     auto impactModel = mappers::toDto(impact);
-
-    co_await impactMapper.insert(impactModel);
+    co_await context.impactMapper.insert(impactModel);
     const int impactId = impactModel.getValueOfId();
 
     for (auto &&jobModel :
          intervals |
              std::views::transform(mappers::toDto.withImpactId(impactId))) {
-        co_await jobsMapper.insert(jobModel);
+        co_await context.jobsMapper.insert(jobModel);
     }
-
-    calendar[scheduler::utils::parseIntToStringID(impactId)] =
-        std::move(schedule);
 }
 
-auto CalendarService::get(const std::string &jobIdString) -> drogon::Task<ScheduleResult> {
-    std::shared_lock lock(mutex);
-
-    auto result = calendar.find(jobIdString);
-    if (result != calendar.end()) {
-        auto [impact, schedule] = result->second;
-        co_return ScheduleResult{.jobId = jobIdString,
-                              .schedule = {schedule.begin(), schedule.end()},
-                              .impact = impact};
-    }
-
-    auto dbPtr = drogon::app().getDbClient();
-    mappers::ImpactMapper impactMapper(dbPtr);
-    mappers::JobMapper jobsMapper(dbPtr);
+auto get(const std::string &jobIdString) -> drogon::Task<ScheduleResult> {
+    auto context = getContex() ;
 
     const int jobIdInt = scheduler::utils::parseStringIDtoInt(jobIdString);
 
-    auto impact = mappers::fromDto(co_await impactMapper.findByPrimaryKey(jobIdInt));
-    auto jobsModels = co_await jobsMapper.findBy(
-        drogon::orm::Criteria(mappers::JobModel::Cols::_impact_id,
-                              drogon::orm::CompareOperator::EQ, jobIdInt));
+    auto [impactModel, jobsModels] = co_await scheduler::coro::when_all(
+        to_task<mappers::ImpactModel>(context.impactMapper.findByPrimaryKey(jobIdInt)),
+        to_task<std::vector<mappers::JobModel>>(
+            context.jobsMapper.findBy(jobImpactIdEqualityCriteria(jobIdInt))));
+
+    auto impact = mappers::fromDto(impactModel);
     auto jobs = mappers::fromDtoAll(jobsModels);
 
-    auto stringJobId = scheduler::utils::parseIntToStringID(jobIdInt);
-    calendar[stringJobId] = {impact, {jobs.begin(), jobs.end()}};
-
     co_return ScheduleResult{
-        .jobId = stringJobId, .schedule = jobs, .impact = impact};
+        .jobId = jobIdString, .schedule = jobs, .impact = impact};
 }
 
-auto CalendarService::get() -> drogon::Task<Calendar> {
-    std::shared_lock lock(mutex);
-    auto dbPtr = drogon::app().getDbClient();
-    mappers::ImpactMapper impactMapper(dbPtr);
-    mappers::JobMapper jobsMapper(dbPtr);
+auto get() -> drogon::Task<Calendar> {
+    auto context = getContex();
 
-    for (const auto &impactDto : co_await impactMapper.findAll()) {
+    auto [impactDtos, allJobsDtos] = co_await scheduler::coro::when_all(
+        to_task<std::vector<mappers::ImpactModel>>(context.impactMapper.findAll()),
+        to_task<std::vector<mappers::JobModel>>(context.jobsMapper.findAll()));
+
+    Calendar calendar;
+
+    for (const auto &impactDto : impactDtos) {
         auto impact = mappers::fromDto(impactDto);
         calendar[scheduler::utils::parseIntToStringID(impactDto.getValueOfId())]
             .first = impact;
     }
-    auto allJobs = mappers::fromDtoAll(co_await jobsMapper.findAll());
-    for (const auto &job : allJobs) {
+
+    for (const auto &job :
+         allJobsDtos | std::views::transform(mappers::fromDto)) {
         calendar[job.jobId].second.insert(job);
     }
+
     co_return calendar;
 }
 
-auto CalendarService::deleteSchedule(const std::string &jobId) ->drogon::Task<> {
-    auto dbPtr = drogon::app().getDbClient();
-    mappers::ImpactMapper impactMapper(dbPtr);
-    co_await impactMapper.deleteByPrimaryKey(
+auto deleteSchedule(const std::string &jobId) -> drogon::Task<> {
+    auto context = getContex();
+    co_await context.impactMapper.deleteByPrimaryKey(
         scheduler::utils::parseStringIDtoInt(jobId));
     // we have cascade on delete, so no need to delete the children manually
-    calendar.erase(jobId);
 }
+
+} // namespace calendarService
--- scheduler/src/Calendar.hpp
+++ scheduler/src/Calendar.hpp
@@ -5,13 +5,16 @@
 #include <Scheduler.hpp>
 #include <models/Impacts.h>
 #include <models/Jobs.h>
-#include <shared_mutex>
 #include <structs/ScheduleBlock.hpp>
 
-class CalendarService {
-  public:
+namespace calendarService {
+
+    template<typename T>
+    auto to_task(auto awaitable) -> drogon::Task<T> { co_return co_await awaitable; }
+
     using Schedule = std::pair<ScheduleImpact, std::set<ScheduleBlock>>;
     using Calendar = std::map<std::string, Schedule>;
+
     auto add(Schedule schedule) -> drogon::Task<>;
 
     auto get(const std::string &jobId)
@@ -21,17 +24,6 @@ class CalendarService {
 
     auto deleteSchedule(const std::string &jobId)->drogon::Task<>;
 
-    CalendarService() = default;
-    CalendarService(const CalendarService &) = delete;
-    CalendarService(CalendarService &&) = delete;
-    auto operator=(const CalendarService &) -> CalendarService & = delete;
-    auto operator=(CalendarService &&) -> CalendarService & = delete;
-
-  private:
-    Calendar calendar;
-    mutable std::shared_mutex mutex;
 };
-
-inline CalendarService calendarService;
 
 #endif
