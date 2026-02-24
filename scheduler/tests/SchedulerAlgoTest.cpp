f96a898900cc64d5a5e51312604c6a4fc89e39c4
akioweh <0@akioweh.com>
2026-02-24 13:04:00 +0000

    fix(db): remove total_load and correctly configure tests for arch linux preset

--- scheduler/tests/SchedulerAlgoTest.cpp
+++ scheduler/tests/SchedulerAlgoTest.cpp
@@ -1,6 +1,6 @@
 #define BOOST_TEST_MODULE SchedulerAlgoTest
 #include <SchedulerAlgo.hpp>
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #include <cmath>
 #include <numeric>
 
--- scheduler/tests/UtilsTest.cpp
+++ scheduler/tests/UtilsTest.cpp
@@ -1,6 +1,6 @@
 #define BOOST_TEST_MODULE UtilsTest
 #include "utils/Utils.hpp"
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #include <chrono>
 
 using namespace scheduler::utils;
