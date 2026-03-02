b6bcc74a6c069871090542e4d66dfac8d99ce616
arjafree <arjafree@gmail.com>
2026-03-02 18:29:21 +0000

    south yorkshire

--- stats/db_utils.py
+++ stats/db_utils.py
@@ -17,7 +17,7 @@ CARBON_DB_FILE = Path(os.environ.get(
 UK_REGION_TO_DC = {
     13: "Data-Center-1",  # London
     14: "Data-Center-2",  # South East England
-    2: "Data-Center-3",   # South Scotland
+    5: "Data-Center-3",   # South Yorkshire
     3: "Data-Center-4",   # North West England
     4: "Data-Center-5",   # North East England
 }
