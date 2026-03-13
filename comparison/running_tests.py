87a3cbbfb90487e117182e9255c096522d0a3628
maksiu <maksymilian.sieklinski.24@ucl.ac.uk>
2026-03-13 20:47:35 +0000

    made the length of compute actually valid

--- comparison/running_tests.py
+++ comparison/running_tests.py
@@ -15,22 +15,27 @@ def power_coeficients() -> list[float]:
     return [1, 0.5]
 
 
-def calc_length_of_computation(workload: float, power_mode: float) -> float:
-    return workload / power_mode
+def calc_length_of_computation(
+    workload: float, fullPower: float, power_mode: float
+) -> float:
+    return workload / (fullPower * power_mode)
 
 
 def run_single_test(
     test_config: TestConfig,
 ):
-    start_time = "2026-03-13T18:00:00Z"
-    end_time = "2026-03-14T17:00:00Z"
+    start_time = "2026-03-13T20:00:00Z"
+    end_time = "2026-03-14T19:00:00Z"
     results: dict[tuple[float, float], tuple[float, float]] = {}
     for workload in get_workload_amounts():
         for power_mode in power_coeficients():
             print("running", workload, power_mode)
             sdkApi = SDKApiManager(start_time, end_time)
             ourApi = OurApiManager(start_time, end_time)
-            length_of_computation = calc_length_of_computation(workload, power_mode)
+            full_power: float = test_config["full_power"]
+            length_of_computation = calc_length_of_computation(
+                workload, full_power, power_mode
+            )
             sdkCarbonEmissions = (
                 workload
                 * sdkApi.get_min_carbon_intensity_over_locaions(length_of_computation)
