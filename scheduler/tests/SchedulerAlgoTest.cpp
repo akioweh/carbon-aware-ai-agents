1845a71bf8bf7e6a3dbe2bc9fdffaa36e5bc4a09
akioweh <0@akioweh.com>
2026-04-06 16:04:55 +0100

    reorganize includes

--- scheduler/tests/SchedulerAlgoTest.cpp
+++ scheduler/tests/SchedulerAlgoTest.cpp
@@ -1,6 +1,6 @@
-#include "Constants.hpp"
 #define BOOST_TEST_MODULE SchedulerAlgoTest
 
+#include "Constants.hpp"
 #include <SchedulerAlgo.hpp>
 #include <boost/test/unit_test.hpp>
 #include <cmath>
